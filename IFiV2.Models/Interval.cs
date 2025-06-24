83d3a43a80bf4dd1cc7d2f1bd38461a7bffc7953
Nick Claerhout <claerhout.nick@gmail.com>
2025-06-25 00:01:17 +0200

    refactoring

--- IFiV2.Models/Interval.cs
+++ IFiV2.Models/Interval.cs
@@ -14,4 +14,20 @@ namespace IFiV2.Models
         _1h, 
         _1d
     }
+
+    public static class IntervalExtensions
+    {
+        public static TimeSpan ToTimeSpan(this Interval interval)
+        {
+            return interval switch
+            {
+                Interval._1m => TimeSpan.FromMinutes(1),
+                Interval._5m => TimeSpan.FromMinutes(5),
+                Interval._15m => TimeSpan.FromMinutes(15),
+                Interval._1h => TimeSpan.FromHours(1),
+                Interval._1d => TimeSpan.FromDays(1),
+                _ => throw new ArgumentOutOfRangeException(nameof(interval), interval, null)
+            };
+        }
+    }
 }
